e36ce6fca5e7db1fa4ae8fea8ad048b4ed386bf7
Pol-S <plsbnjy@gmail.com>
2020-06-29 19:55:40 -0500

    Added a fullscreen button

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullscreen = player.querySelector(".fullscreen");
 
 /*functions */
 function togglePlay() {
@@ -40,11 +41,17 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  console.log("fullscreen attempt");
+  video.requestFullscreen();
+}
+
 /* Hook up the event listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 video.addEventListener("timeupdate", handleProgress);
+fullscreen.addEventListener("click", toggleFullscreen);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
